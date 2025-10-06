5c4a3464bbef7b94b15df2891378ce8892e614e6
Rahul Razz <rahulrajln1111@gmail.com>
2025-10-06 14:46:46 +0530

    Revert "Adding process management"
    This reverts commit caeeb6db09269c3d03a66f124656529bcf3f3536.

--- src/user/test.c
+++
@@ -1,6 +0,0 @@
-
-void user_program_test(void) {
-    kputs("Hello from user-space!\n");
-    // This infinite loop is a simple way to prevent a crash
-    for (;;) {} 
-}
--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,8 @@
-#ifndef UTILS_H
-#define UTILS_H
+#ifndef UTIL_H
+#define UTIL_H
 
 #include <stdint.h>
 
 void int_to_hex(uint32_t n, char *out);
-void kputhex(uint32_t x);
-void kputs(const char* s);
+
 #endif
